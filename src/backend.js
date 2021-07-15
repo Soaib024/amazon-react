989fe6a9c1fac299b866f46f9399d96cb8380e9e
soaib <soaib024@gmail.com>
2021-07-15 09:07:31 +0530

    backend-api

--- src/backend.js
+++ src/backend.js
@@ -1,2 +1,2 @@
-//export const API = "https://backend-amazon.herokuapp.com/api";
-export const API = "http://localhost:5000/api";
+export const API = "https://amazon-node.herokuapp.com/api";
+//export const API = "http://localhost:5000/api";
